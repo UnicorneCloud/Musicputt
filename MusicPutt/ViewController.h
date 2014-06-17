44d14c9fe0f4e004e04ee9dffa8576a165b9e8a8
Eric Pinet <eric.pinet@imagemsoft.com>
2014-06-16 20:07:53 -0400

    Project creation.

---
+++ MusicPutt/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  MusicPutt
+//
+//  Created by Eric Pinet on 2014-06-16.
+//  Copyright (c) 2014 Eric Pinet. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ MusicPuttTests/MusicPuttTests.m
@@ -0,0 +1,34 @@
+//
+//  MusicPuttTests.m
+//  MusicPuttTests
+//
+//  Created by Eric Pinet on 2014-06-16.
+//  Copyright (c) 2014 Eric Pinet. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface MusicPuttTests : XCTestCase
+
+@end
+
+@implementation MusicPuttTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
