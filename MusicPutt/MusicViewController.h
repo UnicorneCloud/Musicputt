b4a838044c99570e61f7b30086e30868b90f0f2a
Eric Pinet <pineri01@gmail.com>
2014-06-25 22:07:28 -0400

    NewDesign Tests (2)

--- MusicPutt/MusicViewController.h
+++ MusicPutt/MusicViewController.h
@@ -7,12 +7,8 @@
 //
 
 #import <UIKit/UIKit.h>
-#import "UIViewArtwork.h"
-#import "UIViewCurrentTrackDetails.h"
 
 @interface MusicViewController : UIViewController
 
-@property (weak, nonatomic) IBOutlet UIViewArtwork* artwork;
-@property (weak, nonatomic) IBOutlet UIViewCurrentTrackDetails* currentTrack;
 
 @end
