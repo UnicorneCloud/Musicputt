521bffc112927320145cef831fdd30d050de9a06
Eric Pinet <pineri01@gmail.com>
2014-08-02 12:27:28 -0400

    UIViewControllerMusic change public members to private.

--- MusicPutt/UIViewControllerMusic.m
+++ MusicPutt/UIViewControllerMusic.m
@@ -14,6 +14,8 @@
 #import "UIViewControllerAlbumPage.h"
 #import "UIViewControllerLyricsPage.h"
 #import "UIButton+Extensions.h"
+#import "UIImageViewArtwork.h"
+
 
 #import <AVFoundation/AVFoundation.h>
 #import <MediaPlayer/MPMusicPlayerController.h>
@@ -27,8 +29,165 @@
     NSTimer *timer;
 }
 
+/**
+ *  App delegate
+ */
 @property AppDelegate* del;
+
+/**
+ *  Data manager of musicputt app
+ */
 @property MPDataManager *datamanager;
+
+/**
+ *  Page view controller for access artist, album and lyrics.
+ */
+@property (weak, nonatomic) IBOutlet UIPageViewController*  pageviewcontroller;
+
+/**
+ *  View that content page view controller for artist, album and lyrics.
+ */
+@property (weak, nonatomic) IBOutlet UIView*            pageview;
+
+/**
+ *  Artwork page.
+ */
+@property (strong, nonatomic) UIViewControllerArtworkPage* artworkpage;
+
+/**
+ *  Album details page.
+ */
+@property (strong, nonatomic) UIViewControllerAlbumPage* albumpage;
+
+/**
+ *  Artist information page. (Not display right now)
+ */
+@property (strong, nonatomic) UIViewControllerArtistPage* artistpage;
+
+/**
+ *  Lyrics page.
+ */
+@property (strong, nonatomic) UIViewControllerLyricsPage* lyricspage;
+
+/**
+ *  Artwork of the current playing song.
+ */
+@property (weak, nonatomic) IBOutlet UIImageView*       imageview;
+
+/**
+ *  Page control to display page.
+ */
+@property (weak, nonatomic) IBOutlet UIPageControl*     pagecontrol;
+
+/**
+ *  Song title of the current playing song.
+ */
+@property (weak, nonatomic) IBOutlet UILabel*           songtitle;
+
+/**
+ *  Artist and Album title of the current playing song.
+ */
+@property (weak, nonatomic) IBOutlet UILabel*           artistalbum;
+
+/**
+ *  View contain all controls to manage media player.
+ */
+@property (weak, nonatomic) IBOutlet UIView*            controlview;
+
+/**
+ *  Current playing time. Position in the current playing song.
+ */
+@property (weak, nonatomic) IBOutlet UILabel*           curtime;
+
+/**
+ *  Elapse time to reach end of the current playing song.
+ */
+@property (weak, nonatomic) IBOutlet UILabel*           endtime;
+
+/**
+ *  Progressbar to display current position in the current playing song.
+ */
+@property (weak, nonatomic) IBOutlet UIProgressView*    progresstime;
+
+/**
+ *  Button to display and manage shuffle mode of the media player.
+ */
+@property (weak, nonatomic) IBOutlet UIButton*          shuffle;
+
+/**
+ *  Button to display and manage the repeat mode of the media player.
+ */
+@property (weak, nonatomic) IBOutlet UIButton*          repeat;
+
+/**
+ *  Button to manage the rewind for the media player.
+ */
+@property (weak, nonatomic) IBOutlet UIButton*          rewind;
+
+/**
+ *  Play/Pause button for the media player.
+ */
+@property (weak, nonatomic) IBOutlet UIButton*          playpause;
+
+/**
+ *  Button to manage fastfoward for the media player.
+ */
+@property (weak, nonatomic) IBOutlet UIButton*          fastfoward;
+
+
+/**
+ *  Button to manage share functionality
+ */
+@property (weak, nonatomic) IBOutlet UIButton*          share;
+
+/**
+ *  Menu bar on top of the Artwork for display lyrics, artist, album, discover, share.
+ */
+@property (weak, nonatomic) IBOutlet UIView*            menubar;
+
+
+/**
+ *  Shuffle button pressed by the user.
+ *
+ *  @param sender sender of event.
+ */
+//- (IBAction)shufflePressed:(id)sender;
+
+/**
+ *  Repeat button pressed by the user.
+ *
+ *  @param sender sender of event.
+ */
+//- (IBAction)repeatPressed:(id)sender;
+
+/**
+ *  Rewind button pressed by the user.
+ *
+ *  @param sender send of event.
+ */
+//- (IBAction)rewindPressed:(id)sender;
+
+/**
+ *  Play/Pause button pressed by the user.
+ *
+ *  @param sender sender of event.
+ */
+//- (IBAction)playpausePressed:(id)sender;
+
+/**
+ *  Foward button pressed by the user.
+ *
+ *  @param sender sender of event.
+ */
+//- (IBAction)fastFowardPressed:(id)sender;
+
+
+/**
+ *  Share button was pressed by the user.
+ *
+ *  @param sender sender of event.
+ */
+//- (IBAction)sharePressed:(id)sender;
 
 @end
 
