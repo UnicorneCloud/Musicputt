e808f45beb6c9b7d9fbb7b4ffc58257fbb233172
Eric Pinet <pineri01@gmail.com>
2014-08-03 01:02:35 -0400

    Documentation update.

--- MusicPutt/MPArtist.h
+++ MusicPutt/MPArtist.h
@@ -14,23 +14,49 @@
  */
 @interface MPArtist : NSObject
 
-
+/**
+ *  wrapper Type.
+ */
 @property (nonatomic, copy) NSString *wrapperType;
 
+/**
+ *  Artist Type.
+ */
 @property (nonatomic, copy) NSString *artistType;
 
+/**
+ *  Artist Name.
+ */
 @property (nonatomic, copy) NSString *artistName;
 
+/**
+ *  Artist link url in the iTunes Store.
+ */
 @property (nonatomic, copy) NSString *artistLinkUrl;
 
+/**
+ *  Artist Id in the iTunes Store.
+ */
 @property (nonatomic, copy) NSString *artistId;
 
+/**
+ *  Artist AMG ID in the iTunes Store.
+ */
 @property (nonatomic, copy) NSString *amgArtistId;
 
+/**
+ *  Artist primary genre name.
+ */
 @property (nonatomic, copy) NSString *primaryGenreName;
 
+/**
+ *  Artist primary genre id.
+ */
 @property (nonatomic, copy) NSString *primaryGenreId;
 
+/**
+ *  Artist radio url.
+ */
 @property (nonatomic, copy) NSString *radioStationUrl;
 
 @end
