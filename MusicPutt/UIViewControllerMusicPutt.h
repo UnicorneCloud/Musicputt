81c0520c5e8e629ab337a1a5a535cdb6bae3a25d
Eric Pinet <pineri01@gmail.com>
2015-04-19 16:36:13 -0400

    Create file to musicputt main page.

---
+++ MusicPutt/UIViewControllerMusicPutt.h
@@ -0,0 +1,13 @@
+//
+//  UIViewControllerMusicPutt.h
+//  MusicPutt
+//
+//  Created by Eric Pinet on 2015-04-19.
+//  Copyright (c) 2015 Eric Pinet. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIViewControllerMusicPutt : UIViewController
+
+@end
